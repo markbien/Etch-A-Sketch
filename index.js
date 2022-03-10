c756dedee498b3561b8278c4a8ac1b91149ab55c
Mark Danielle Bien <mark.bien@outlook.com>
2022-03-10 17:03:59 +0800

    Create functions for recalls

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
 // CREATE 16x16 SQUARE DIVS
 const bod = document.querySelector("body");
-const main = document.createElement("main");
 let squaresPerSide = 16;
-bod.appendChild(main);
+
 const resetButton = document.createElement("button");
 resetButton.textContent = "Reset";
 bod.appendChild(resetButton);
 let isDrawing = false;
-
-// CREATE 16 MAIN DIVS AND APPEND TO bod
-for (let j = 1; j <= squaresPerSide; j++) {
-  const mainDiv = document.createElement("div");
-  mainDiv.classList.add("container");
-  mainDiv.classList.add(`container-${j}`);
-
-  // APPEND mainDiv TO bod
-  main.appendChild(mainDiv);
-
-  // CREATE 16 DIVS AND APPEND TO container1
-  for (let i = 1; i <= squaresPerSide; i++) {
-    const div = document.createElement("div");
-    div.classList.add(`sub-container`);
-    // APPEND div TO mainDiv
-    mainDiv.appendChild(div);
-  }
-}
-
-const container = document.querySelectorAll('.container');
-const size = 960;
-container.forEach(cont => {
-    cont.style.height = (960/squaresPerSide) + "px";
-})
-
-
-// EVENT LISTENER
-// SELECT THE BOXES
-const subContainers = document.querySelectorAll(".sub-container");
-main.addEventListener('mousedown', ()=> isDrawing = true);
-main.addEventListener('mouseup', ()=> isDrawing = false);
-main.addEventListener('mouseleave', ()=> isDrawing = false);
-
-subContainers.forEach((sub) => {
-  sub.addEventListener("mouseover", handleEvent);
-});
 
 resetButton.addEventListener("click", clearAllBoxes);
 
@@ -54,5 +17,63 @@ function handleEvent() {
 }
 
 function clearAllBoxes() {
+//   createContainer();
+  const subContainers = document.querySelectorAll('.sub-container');
   subContainers.forEach((sub) => sub.classList.remove("color"));
 }
+
+function createMain() {
+  const main = document.createElement("main");
+  bod.appendChild(main);
+}
+
+function createContainer() {
+  // CREATE 16 MAIN DIVS AND APPEND TO bod
+  for (let j = 1; j <= squaresPerSide; j++) {
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("container");
+    mainDiv.classList.add(`container-${j}`);
+
+    // APPEND mainDiv TO bod
+    const main = document.querySelector("main");
+    main.appendChild(mainDiv);
+
+    // CREATE 16 DIVS AND APPEND TO container1
+    for (let i = 1; i <= squaresPerSide; i++) {
+      const div = document.createElement("div");
+      div.classList.add(`sub-container`);
+      // APPEND div TO mainDiv
+      mainDiv.appendChild(div);
+    }
+  }
+}
+
+function assignSize() {
+  // ASSIGNS THE SIZE OF THE CONTAINERS
+  const container = document.querySelectorAll(".container");
+  const size = 960;
+  container.forEach((cont) => {
+    cont.style.height = 960 / squaresPerSide + "px";
+  });
+}
+
+function createEventListeners() {
+  const main = document.querySelector("main");
+
+  // EVENT LISTENER
+  // SELECT THE BOXES
+  const subContainers = document.querySelectorAll(".sub-container");
+  main.addEventListener("mousedown", () => (isDrawing = true));
+  main.addEventListener("mouseup", () => (isDrawing = false));
+  main.addEventListener("mouseleave", () => (isDrawing = false));
+
+  subContainers.forEach((sub) => {
+    sub.addEventListener("mouseover", handleEvent);
+  });
+}
+
+// test
+createMain();
+createContainer();
+assignSize();
+createEventListeners();
