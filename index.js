6652be253be77cd5c73ea5d14783e5094e970ba1
Mark Danielle Bien <mark.bien@outlook.com>
2022-03-10 16:31:31 +0800

    Add variable for container size and squaresPerSide

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // CREATE 16x16 SQUARE DIVS
 const bod = document.querySelector("body");
 const main = document.createElement("main");
+let squaresPerSide = 16;
 bod.appendChild(main);
 const resetButton = document.createElement("button");
 resetButton.textContent = "Reset";
@@ -8,7 +9,7 @@ bod.appendChild(resetButton);
 let isDrawing = false;
 
 // CREATE 16 MAIN DIVS AND APPEND TO bod
-for (let j = 1; j <= 32; j++) {
+for (let j = 1; j <= squaresPerSide; j++) {
   const mainDiv = document.createElement("div");
   mainDiv.classList.add("container");
   mainDiv.classList.add(`container-${j}`);
@@ -17,13 +18,20 @@ for (let j = 1; j <= 32; j++) {
   main.appendChild(mainDiv);
 
   // CREATE 16 DIVS AND APPEND TO container1
-  for (let i = 1; i <= 32; i++) {
+  for (let i = 1; i <= squaresPerSide; i++) {
     const div = document.createElement("div");
     div.classList.add(`sub-container`);
     // APPEND div TO mainDiv
     mainDiv.appendChild(div);
   }
 }
+
+const container = document.querySelectorAll('.container');
+const size = 960;
+container.forEach(cont => {
+    cont.style.height = (960/squaresPerSide) + "px";
+})
+
 
 // EVENT LISTENER
 // SELECT THE BOXES
